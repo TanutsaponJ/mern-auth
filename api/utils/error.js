dacdf248d2e1b7c5b64c9a0ab213fcea69b209bf
TanutsaponJ <anakin112333@gmail.com>
2023-11-14 13:14:47 -0500

    add a middleware and a function to handle errors

---
+++ api/utils/error.js
@@ -0,0 +1,9 @@
+// Desc: Error handler for the API
+
+export const errorHandler = (statusCode, message) => {
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+}
+
